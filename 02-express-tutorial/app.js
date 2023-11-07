545d548d79f8c532b5ac65fc9182e6ea14234697
Beaskyy <afeezakintobi@nqb8.co>
2023-11-07 20:44:15 +0100

    navbar

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,23 @@
 const { readFileSync } = require("fs");
 const { createServer } = require("http");
 
-const homepage = readFileSync("./index.html")
+const homepage = readFileSync("./navbar-app/index.html");
+const stylesFile = readFileSync("./navbar-app/styles.css");
+const logo = readFileSync("./navbar-app/logo.svg");
+const menu = readFileSync("./navbar-app/browser-app.js");
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html"});
-  res.write(homepage)
-  res.end();
-})
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homepage);
+    res.end();
+  } else if (req.url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(stylesFile);
+    res.end();
+  }
+});
 
 server.listen(5000, () => {
-  console.log('server listening on port 5000')
-})
+  console.log("server listening on port 5000");
+});
