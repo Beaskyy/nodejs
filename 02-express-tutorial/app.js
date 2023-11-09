1f43342969ddf786c552c2d667b02841c2ba985b
Beaskyy <afeezakintobi@nqb8.co>
2023-11-09 20:15:03 +0100

    remove about

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,9 +6,7 @@ app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About page");
-})
+
 
 app.listen(5000, () => {
   console.log("server listening on port 5000...");
