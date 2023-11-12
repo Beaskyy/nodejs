0324fe74f8e6cb3ebdc1e5b7dadee41487754641
Beaskyy <afeezakintobi@nqb8.co>
2023-11-12 21:29:10 +0100

    search query

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,29 +4,51 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Homepage</h1><a href='/api/products'>Products</a>`);
+  res.send(`<h1>Home page</h1><a href="/api/products">Products</a>`);
 });
 
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
-
     return { id, name, image };
   });
   res.json(newProducts);
 });
 
 app.get("/api/products/:id", (req, res) => {
-  const {id} = req.params
-  const singleProduct = products.find(
-    (product) => product.id === +id
-  );
-  if(!singleProduct) {
-    return res.status(404).send("Product not found")
+  const { id } = req.params;
+  const singleProduct = products.find((product) => product.id === +id);
+  if (!singleProduct) {
+    return res.status(404).send("Page not found");
   }
-  return res.json(singleProduct);
+  res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) =>
+      product.name.startsWith(search)
+    );
+
+    return res.json(sortedProducts);
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, +limit);
+    return res.json(sortedProducts);
+  }
+
+  if (sortedProducts.length < 1) {
+    return res.status(404).json({ success: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
-  console.log(`server listening on port 5000...`);
+  console.log("server listening on port 5000...");
 });
