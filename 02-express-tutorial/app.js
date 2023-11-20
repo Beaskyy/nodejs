0faacd292c4d26f043fd940bf4aaa5abb50fc762
Beaskyy <afeezakintobi@nqb8.co>
2023-11-20 21:41:48 +0100

    delete request

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -59,6 +59,22 @@ app.put("/api/people/:id", (req, res) => {
   res.status(200).json({ success: true, data: newPeople });
 });
 
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+
+  const person = people.find((person) => person.id === +id);
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `Cannot find person with id ${id}` });
+  }
+
+  const newPeople = people.filter((person) => person.id !== +id);
+
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
